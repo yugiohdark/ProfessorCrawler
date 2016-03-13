ec263ac40fc46a7369176cd5c64c1d9d66c9d24d
yugiohdark <yugiohdark@gmail.com>
2016-03-12 23:59:01 -0600

    uiuc

--- tutorial/spiders/uiuc_spider.py
+++ tutorial/spiders/uiuc_spider.py
@@ -1,17 +1,29 @@
 import scrapy
 from tutorial.items import ProfessorItem
-
+from selenium import webdriver
+import time
 
 class UIUCSpider(scrapy.Spider):
     name = "uiuc"
     start_urls = [
-        "https://www.cs.utexas.edu/faculty",
+        "https://cs.illinois.edu/directory/faculty?quicktabs_faculty_tabs_new=0#quicktabs-faculty_tabs_new",
     ]
 
+    def __init__(self):
+        scrapy.Spider.__init__(self)
+        # use any browser you wish
+        self.driver = webdriver.Firefox() 
+    def __del__(self):
+        self.driver.close()
+
     def parse(self, response):
-        for sel in response.css('.views-row'):
-            profName = sel.css('.prof-name::text').extract().strip()
-            if profName:
-                item = ProfessorItem()
-                item['name'] = profName
-                yield item
+        #start browser
+        self.driver.get(response.url)
+        #loading time interval
+        time.sleep(5)
+        # get the data and write it to scrapy items
+        for sel in self.driver.find_elements_by_css_selector('#quicktabs_tabpage_faculty_tabs_new_0 .extDirectoryPerson'):
+            item = ProfessorItem()
+            item['name'] = sel.find_element_by_css_selector('.extDirectoryName a').text
+            yield item
+        self.driver.close()
